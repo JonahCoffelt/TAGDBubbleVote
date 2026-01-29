bebc695a780456798d920bc8e54104218cf92953
Jonah Coffelt <coffelt.jonah0918@gmail.com>
2026-01-29 17:29:13 -0600

    chore: generalized ip. added QR code gen

--- host.py
+++ host.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify, render_template_string
+from QRCodeGenerator import generate_qr_code
+import socket
 
 app = Flask(__name__)
 
@@ -29,5 +31,35 @@ def vote():
 def get_votes():
     return jsonify(votes)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+def get_local_url(port=5000):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # This never actually sends packets, it just forces IP selection
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return f"http://{ip}:{port}"
+
+# File settings
+url = get_local_url()      # The link the qr code to sends users to
+qr_code_name = "TeamsCode"     # The name of the image generated
+
+print(url)
+
+# Resolution and padding
+box_size = 15                   # How many pixels go in one box
+border = 3                      # How many boxes of padding it contain out the edges
+
+# Colors                        Acceptable inputs
+light_color = (255, 255, 255)   # RGB tuple: (000, 123, 255)
+dark_color = "#000000"          # HEX string: "#123456" or "123456"
+                                # Or any valid python color names: "white", "darkblue", etc.
+# Center image
+image_name = "logo.png"          # Include directory and file type (Ex: "Logos/MyPicture.png")
+img_size = 2                    # How large is the center image?
+correction = 4                  # 1 has the least correction and 4 has the most
+
+generate_qr_code(url, qr_code_name, box_size, border, light_color, dark_color, image_name, img_size, correction)
+
+app.run(host="0.0.0.0", port=5000, debug=True)
